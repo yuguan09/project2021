ae68d3301bbdf8745a40635605b9a4e78ffa20f0
yuguan09 <38544697+yuguan09@users.noreply.github.com>
2021-03-16 19:58:57 +0900

    Add files via upload

---
+++ 0X01.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
+
+#define MAX 256
+#define ALPHA 26
+
+void main(void)
+{
+    char str[MAX];
+    int alpha[ALPHA];
+    int i;
+
+    memset(&alpha[0],'\0',sizeof alpha );//初期化
+    for ( ; scanf("%s",&str[0]) !=EOF; )
+    {
+        for ( i = 0; str[i]!='\0'; i++)
+        {
+            if(isalpha(str[i]))
+            {
+                str[i]=(char)toupper(str[i]);
+                (alpha[str[i]-'A'])++;
+            }
+        }
+    }
+        for ( i = 0; i < ALPHA; i++)
+        {
+            if((i%5)==0)
+            {
+                putchar('\n');
+            }
+            printf("%c=%5d",i+'A',alpha[i]);/* code */
+        }
+     putchar('\n');    
+}
